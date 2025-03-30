606764b863ab40023394e6d3b0cd0af962d61c10
Luka Knezic <luka@giscloud.com>
2025-03-31 01:02:49 +0200

    Move business logic from controller to service

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,46 +4,17 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Dto\Auth\TokenIssueRequest;
-use App\Dto\Auth\TokenIssueResponse;
 use App\Entity\Token;
-use App\Entity\User;
-use App\Exception\BadRequestException;
 use App\Response\JsonResponse;
+use App\Service\TokenIssueService;
 use App\System\Auth;
-use RuntimeException;
 
 final class AuthController
 {
     public static function issueToken(TokenIssueRequest $tokenIssueRequest): JsonResponse
     {
-        $user = (new User())->hydrate([
-            'username' => $tokenIssueRequest->username,
-            'secret_hash' => password_hash($tokenIssueRequest->secret, \PASSWORD_BCRYPT),
-        ]);
-        try {
-            $user->save();
-        } catch (RuntimeException $e) {
-            throw new BadRequestException('Failed to save user');
-        }
-        if ($user->id === null) {
-            throw new RuntimeException('Failed to save user');
-        }
-
-        $token = Token::getToken();
-        $token->user_id = $user->id;
-        $token->description = $tokenIssueRequest->description;
-        $token->expires_at = $tokenIssueRequest->expires;
-        $token->save();
-        if ($token->id === null || $token->getUnhashedToken() === null) {
-            throw new RuntimeException('Failed to save token');
-        }
-
-        $tokenResponse = new TokenIssueResponse(
-            $token->token_id,
-            $token->getUnhashedToken(),
-            $tokenIssueRequest->description,
-            $tokenIssueRequest->expires
-        );
+        $tokenIssueService = new TokenIssueService();
+        $tokenResponse = $tokenIssueService->issueToken($tokenIssueRequest);
 
         return new JsonResponse($tokenResponse->toArray());
     }
---
+++ src/Service/TokenIssueService.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Dto\Auth\TokenIssueRequest;
+use App\Dto\Auth\TokenIssueResponse;
+use App\Entity\Token;
+use App\Entity\User;
+use App\Exception\BadRequestException;
+use RuntimeException;
+
+final class TokenIssueService
+{
+    public function issueToken(TokenIssueRequest $tokenIssueRequest): TokenIssueResponse
+    {
+        $user = (new User())->hydrate([
+            'username' => $tokenIssueRequest->username,
+            'secret_hash' => password_hash($tokenIssueRequest->secret, \PASSWORD_BCRYPT),
+        ]);
+        try {
+            $user->save();
+        } catch (RuntimeException $e) {
+            throw new BadRequestException('Failed to save user');
+        }
+        if ($user->id === null) {
+            throw new RuntimeException('Failed to save user');
+        }
+
+        $token = Token::getToken();
+        $token->user_id = $user->id;
+        $token->description = $tokenIssueRequest->description;
+        $token->expires_at = $tokenIssueRequest->expires;
+        $token->save();
+        if ($token->id === null || $token->getUnhashedToken() === null) {
+            throw new RuntimeException('Failed to save token');
+        }
+
+        return new TokenIssueResponse(
+            $token->token_id,
+            $token->getUnhashedToken(),
+            $tokenIssueRequest->description,
+            $tokenIssueRequest->expires
+        );
+    }
+}
